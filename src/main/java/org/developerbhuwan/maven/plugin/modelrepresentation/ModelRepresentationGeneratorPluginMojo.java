5b73e5db59092f0f309ad67945b14242bea5c876
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-03-25 00:03:32 +0400

    Model Representation Generator Plugin Mojo

--- src/main/java/org/developerbhuwan/maven/plugin/modelrepresentation/ModelRepresentationGeneratorPluginMojo.java
+++ src/main/java/org/developerbhuwan/maven/plugin/modelrepresentation/ModelRepresentationGeneratorPluginMojo.java
@@ -1,10 +1,15 @@
 package org.developerbhuwan.maven.plugin.modelrepresentation;
 
+import org.apache.maven.plugins.annotations.Mojo;
 import org.developerbhuwan.maven.core.AbstractPluginMojo;
+
+import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_SOURCES;
+import static org.apache.maven.plugins.annotations.ResolutionScope.TEST;
 
 /**
  * @author Bhuwan Prasad Upadhyay
  */
+@Mojo(name = "generate-java", defaultPhase = GENERATE_SOURCES, requiresDependencyResolution = TEST, requiresDependencyCollection = TEST, threadSafe = true)
 public class ModelRepresentationGeneratorPluginMojo extends AbstractPluginMojo {
 
     @Override
