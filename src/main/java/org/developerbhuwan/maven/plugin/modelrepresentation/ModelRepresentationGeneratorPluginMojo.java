426360d6f17360b0e13b1dd0c0a768a91d621235
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-03-24 23:51:14 +0400

    Model Representation Generator Plugin Mojo

---
+++ src/main/java/org/developerbhuwan/maven/plugin/modelrepresentation/ModelRepresentationGeneratorPluginMojo.java
@@ -0,0 +1,15 @@
+package org.developerbhuwan.maven.plugin.modelrepresentation;
+
+import org.developerbhuwan.maven.core.AbstractPluginMojo;
+
+/**
+ * @author Bhuwan Prasad Upadhyay
+ */
+public class ModelRepresentationGeneratorPluginMojo extends AbstractPluginMojo {
+
+    @Override
+    protected void doExecute() {
+
+    }
+
+}
